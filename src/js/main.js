95703ac502e7f0f2cf384d0c0981b8e2dabf0ad3
rayhanfuadi <oke.rayhan1@gmail.com>
2024-10-05 04:20:33 +0700

    progress 10%

--- src/js/main.js
+++ src/js/main.js
@@ -1,99 +1,99 @@
 var swiper = new Swiper(".centered-slide-carousel-1", {
-       centeredSlides: true,
-       paginationClickable: true,
-       loop: true,
-       spaceBetween: 32,
-       slideToClickedSlide: true,
-       autoplay: {
-       delay: 3100,  // Set waktu otomatis geser ke 3 detik
-       disableOnInteraction: false,  // Agar autoplay tetap berjalan meski ada interaksi
-       },
-       pagination: {
-         el: ".centered-slide-carousel .swiper-pagination",
-         clickable: true,
-       },
-       breakpoints: {
-         1920: {
-           slidesPerView: 3,
-           spaceBetween: 32
-         },
-         1028: {
-           slidesPerView: 2,
-           spaceBetween: 12
-         },
-         990: {
-           slidesPerView: 1,
-           spaceBetween: 0
-         }
-       }
-      });
+  centeredSlides: true,
+  paginationClickable: true,
+  loop: true,
+  spaceBetween: 32,
+  slideToClickedSlide: true,
+  autoplay: {
+    delay: 3100,  // Set waktu otomatis geser ke 3 detik
+    disableOnInteraction: false,  // Agar autoplay tetap berjalan meski ada interaksi
+  },
+  pagination: {
+    el: ".centered-slide-carousel .swiper-pagination",
+    clickable: true,
+  },
+  breakpoints: {
+    1920: {
+      slidesPerView: 4,
+      spaceBetween: 32
+    },
+    1028: {
+      slidesPerView: 4,
+      spaceBetween: 16
+    },
+    240: {
+      slidesPerView: 2,
+      spaceBetween: 12
+    }
+  }
+});
 
 var swiper2 = new Swiper(".centered-slide-carousel-2", {
-       centeredSlides: true,
-       paginationClickable: true,
-       loop: true,
-       spaceBetween: 30,
-       slideToClickedSlide: true,
-       autoplay: {
-       delay: 3200,  // Set waktu otomatis geser ke 3 detik
-       disableOnInteraction: false,  // Agar autoplay tetap berjalan meski ada interaksi
-       },
-       pagination: {
-         el: ".centered-slide-carousel .swiper-pagination",
-         clickable: true,
-       },
-       breakpoints: {
-         1920: {
-           slidesPerView: 3,
-           spaceBetween: 24
-         },
-         1028: {
-           slidesPerView: 2,
-           spaceBetween: 12
-         },
-         990: {
-           slidesPerView: 1,
-           spaceBetween: 0
-         }
-       }
-      });
+  centeredSlides: true,
+  paginationClickable: true,
+  loop: true,
+  spaceBetween: 30,
+  slideToClickedSlide: true,
+  autoplay: {
+    delay: 3200,  // Set waktu otomatis geser ke 3 detik
+    disableOnInteraction: false,  // Agar autoplay tetap berjalan meski ada interaksi
+  },
+  pagination: {
+    el: ".centered-slide-carousel .swiper-pagination",
+    clickable: true,
+  },
+  breakpoints: {
+    1920: {
+      slidesPerView: 4,
+      spaceBetween: 24
+    },
+    1028: {
+      slidesPerView: 4,
+      spaceBetween: 16
+    },
+    240: {
+      slidesPerView: 2,
+      spaceBetween: 12
+    }
+  }
+});
 
 var swiper3 = new Swiper(".centered-slide-carousel-3", {
-       centeredSlides: true,
-       paginationClickable: true,
-       loop: true,
-       spaceBetween: 30,
-       slideToClickedSlide: true,
-       autoplay: {
-       delay: 3300,  // Set waktu otomatis geser ke 3 detik
-       disableOnInteraction: false,  // Agar autoplay tetap berjalan meski ada interaksi
-       },
-       pagination: {
-         el: ".centered-slide-carousel .swiper-pagination",
-         clickable: true,
-       },
-       breakpoints: {
-         1920: {
-           slidesPerView: 3,
-           spaceBetween: 24
-         },
-         1028: {
-           slidesPerView: 2,
-           spaceBetween: 12
-         },
-         990: {
-           slidesPerView: 1,
-           spaceBetween: 0
-         }
-       }
-      });
+  centeredSlides: true,
+  paginationClickable: true,
+  loop: true,
+  spaceBetween: 30,
+  slideToClickedSlide: true,
+  autoplay: {
+    delay: 3300,  // Set waktu otomatis geser ke 3 detik
+    disableOnInteraction: false,  // Agar autoplay tetap berjalan meski ada interaksi
+  },
+  pagination: {
+    el: ".centered-slide-carousel .swiper-pagination",
+    clickable: true,
+  },
+  breakpoints: {
+    1920: {
+      slidesPerView: 4,
+      spaceBetween: 24
+    },
+    1028: {
+      slidesPerView: 4,
+      spaceBetween: 16
+    },
+    240: {
+      slidesPerView: 2,
+      spaceBetween: 12
+    }
+  }
+});
 
 // navHeader scrool
-window.onscroll = function() {
-    const navbar = document.getElementById('navbar');
-    if (window.scrollY > 0) {
-        navbar.classList.add('drop-shadow-drop3'); 
-    } else {
-        navbar.classList.remove('drop-shadow-drop3');
-    }
+window.onscroll = function () {
+  const navbar = document.getElementById('navbar');
+  if (window.scrollY > 0) {
+    navbar.classList.add('drop-shadow-drop3');
+  } else {
+    navbar.classList.remove('drop-shadow-drop3');
+  }
 };
